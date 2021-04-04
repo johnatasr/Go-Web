556627bbf73df1349332318e4a47332e927b3412
unknown <johnatasr@hotmail.com>
2021-04-04 01:28:08 -0300

    some changes

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.16
 require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 var client *redis.Client
@@ -21,7 +22,40 @@ type indexStruct struct {
 	bootstrap string
 }
 
-func handleMain(w http.ResponseWriter, r *http.Request) {
+var store = sessions.NewCookieStore([]byte("G0lAng"))
+
+func handleGetLogin(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", nil)
+}
+
+func handlePostLogin(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	password := r.PostForm.Get("password")
+	session, _ := store.Get(r, "session")
+	session.Values["username"] = username
+	session.Values["password"] = password
+	session.Save(r, w)
+}
+
+func testGetHandler(w http.ResponseWriter, r *http.Request) {
+
+	session, _ := store.Get(r, "session")
+	untyped, ok := session.Values["username"]
+
+	if !ok {
+		return
+	}
+
+	username, ok := untyped.(string)
+	if !ok {
+		return
+	}
+
+	w.Write([]byte(username))
+}
+
+func handleGetMain(w http.ResponseWriter, r *http.Request) {
 	comments, err := client.LRange("comments", 0, 10).Result()
 
 	if err != nil {
@@ -30,8 +64,18 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 
 	date := time.Now()
 	templates.ExecuteTemplate(w, "index.html", date.Format("01-02-2006 15:04:05"))
-	templates.ExecuteTemplate(w, "index.html", comments)
+
+	if comments != nil {
+		templates.ExecuteTemplate(w, "index.html", comments)
+	}
 	//fmt.Fprint(w, "Aqui é a pagina principal")
+}
+
+func handlePostMain(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, "/", 302)
 }
 
 func handleDetais(w http.ResponseWriter, r *http.Request) {
@@ -65,11 +109,19 @@ func main() {
 		Addr: "localhost:6379",
 	})
 
+	staticDir := http.Dir("./static/")
+	fileServer := http.FileServer(staticDir)
+
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleMain).Methods("GET")
+	r.HandleFunc("/login", handleGetLogin).Methods("GET")
+	r.HandleFunc("/login", handlePostLogin).Methods("POST")
+	r.HandleFunc("/", handleGetMain).Methods("GET")
+	r.HandleFunc("/", handlePostMain).Methods("POST")
 	r.HandleFunc("/detalhes", handleDetais).Methods("GET")
 	r.HandleFunc("/contato", handleContact).Methods("GET")
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fileServer))
 
 	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8000", nil))
